ef8aeb1bfe0ea35fb28ab2485fb52fbfcd39d72d
Dominykas Puskorius <dominykas.puskorius0@gmail.com>
2021-12-15 22:12:40 +0200

    php (nebaigta)

--- html_skaitymas.php
+++ html_skaitymas.php
@@ -15,7 +15,26 @@ $kiekis = filesize($naujas);
 echo 'Failo ' . $naujas . ' rezultatas';
 
 $skaitymas = skaityti($naujas);
-spausdinti($skaitymas, $kiekis);
+/*spausdinti($skaitymas, $kiekis);*/
+
+// 2 uzd
+$manoVardas = 'Dominykas';
+$naujas1 = fread($skaitymas, $kiekis);
+
+echo preg_replace("/pasauli/", $manoVardas, $naujas1);
+
+fclose($skaitymas);
+
+// 3 uzd
+$data = ["Dominykas" => 23];
+json_encode($data);
+
+$temp = file_get_contents('duom.json');
+$tempArray = json_decode($temp, true);
+array_push($tempArray, $data);
+$jsonData = json_encode($tempArray);
+print_r($jsonData);
+file_put_contents('duom.json', $jsonData);
 
 
 
