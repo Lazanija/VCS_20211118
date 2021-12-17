46256ec8bb14a5cff2427feb4e404b4a632e48dc
Dominykas Puskorius <dominykas.puskorius0@gmail.com>
2021-12-17 16:02:32 +0200

    OOP uzd

--- index.php
+++ index.php
@@ -203,3 +203,27 @@ $tesla->drive(5);
 echo '<br>';
 echo 'Rida: ' . $tesla->mileage();
 
+// OOP praktines
+echo '<br>';
+echo '<br>';
+
+include "OOP/Bakas.php";
+
+$masina = new Bakas();
+
+$masina->atstumas = 86;
+$masina->greitis = 45;
+
+$masina->piltiKura(50);
+
+echo 'Turim kuro: ' . $masina->kuras() . ' L';
+echo '<br>';
+echo 'Nuvažiavom ' . $masina->atstumas .  ' KM ' . $masina->greitis . ' KM/H greičiu ir liko: ' .
+     round($masina->kuras() - $masina->nuvaziavo(), 2) . ' L kuro.' .
+     ' Sunaudojom ' . round($masina->sanaudos(), 2) . ' L kuro.';
+;
+
+
+
+
+
